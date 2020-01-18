07a1f6554add211ad1bb7b6a5eee112e406a3795
trantien <tientv@cbex.xyz>
2020-01-18 13:09:31 +0700

    upload

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require("express")
-const app = express()
-const port = 3000
+var express = require("express")
+var app = express()
+var port = 3000
 
 var server = require('http').createServer(app);
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
-
-app.use(express.json())
-app.use(express.urlencoded())
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
